51f0662a68ba4ebbc63b4cb030208e37999a4664
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2023-02-16 18:52:07 +0700

    cuda support added

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import torch
 from detect_main import detect_image_no_cmd
 
 print('Starting...')
-detect_image_no_cmd('images/horses.jpg', 'weights/yolov7.pt', True, 640, 'output', torch.device('cpu'))
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.is_available():
+    print(f"{torch.cuda.device_count()} devices available")
+    print('Using GPU')
+else:
+    print('Using CPU')
+detect_image_no_cmd('images/horses.jpg', 'weights/yolov7.pt', True, 640, 'output', torch.device('cuda'))
 print('Done.')
