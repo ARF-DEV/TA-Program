b8a152f611566a314d5a29151d2eb75d74e5cc81
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2023-05-09 15:54:24 +0700

    split file to predict, test, and main

---
+++ predict.py
@@ -0,0 +1,21 @@
+import implementation as model
+import helper
+print('Starting...')
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# if torch.cuda.is_available():
+#     print(f"{torch.cuda.device_count()} devices available")
+# optical_flow_estimation('videos/Stealing108_x264.mp4', 'weights/fastflownet_gtav.pth', testing=False, save_in_rgb=False, save_img=True)
+# # detect_image_no_cmd('videos/Burglary001_x264.mp4', 'weights/yolov7.pt', True, 640, torch.device('cuda'))
+
+pipeline = model.FastFlowYOLOPipeline(
+    'weights/yolov7.pt', 'weights/fastflownet_gtav.pth', 'inference', False, 'test', binary_tresh=200)
+
+# optical_flow_estimation('videos/Stealing108_x264.mp4', 'weights/fastflownet_gtav.pth',
+# testing=False, save_in_rgb=False, save_img=True)
+# pipeline.detect_and_optical_flow(
+# 'videos/Robbery098_x264.mp4')
+helper.apply_to_folders(
+    pipeline, ['resized/*.mp4', 'videos/Stealing*.mp4', 'videos/Robbery*.mp4', 'videos/Burglary*.mp4'], 'Opening')
+
+
+print('Done.')
