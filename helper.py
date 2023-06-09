0b4dd2391a452d03a50ffd01323e73102704c389
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2023-06-09 11:16:20 +0700

    add per_class testing

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@ import glob
 from implementation import FastFlowYOLOPipeline, resize_video
 import pandas as pd
 from pathlib import Path
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sn
 
@@ -55,9 +55,6 @@ def test_data(prediction_folder, dst_folder):
         video_name = ".".join(l.split(" ")[-1].split(".")[0:2])
         path = Path("inference") / prediction_folder / \
             video_name / "fastflow_movement.csv"
-        mean_ps = 0
-        mean_rs = 0
-        mean_fs = 0
         if path.exists():
             image_dir = Path(dst_folder) / (video_name+".png")
             pred = pd.read_csv(path)
@@ -81,9 +78,6 @@ def test_data(prediction_folder, dst_folder):
                 actual['Penting'].iloc[0: min_length], pred['Penting'].iloc[0: min_length])
             fs = f1_score(actual['Penting'].iloc[0: min_length],
                           pred['Penting'].iloc[0: min_length])
-            mean_ps += ps
-            mean_rs += rs
-            mean_fs += fs
             df_all = pd.concat([df_all, pd.DataFrame({
                 "video": [vid_name],
                 "precision": [ps],
@@ -118,3 +112,83 @@ def test_data(prediction_folder, dst_folder):
 
     df_all.to_csv(all_metrics_dst, index=False)
     df_all.describe().to_csv(desc_metrics_dst)
+
+
+def test_data_per_class(prediction_folder, dst_folder):
+    if not (Path(dst_folder) / prediction_folder).exists():
+        (Path(dst_folder) / prediction_folder).mkdir()
+    labels = glob.glob('labels/*')
+    desc_metrics_dst = Path(dst_folder) / "mean_metrics.csv"
+    print(len(labels))
+    df_all = pd.DataFrame(columns=["video", "precision", "recall", "f1"])
+    n = 0
+    mean_tps = 0
+    mean_trs = 0
+    mean_tfs = 0
+    mean_fps = 0
+    mean_frs = 0
+    mean_ffs = 0
+    mean_wfs = 0
+    mean_wrs = 0
+    mean_wps = 0
+    for l in labels:
+        video_name = ".".join(l.split(" ")[-1].split(".")[0:2])
+        path = Path("inference") / prediction_folder / \
+            video_name / "fastflow_movement.csv"
+        if path.exists():
+            pred = pd.read_csv(path)
+            actual = pd.read_csv(l)
+            print(f"pred path: {path}")
+            print(f"actual path: {l}")
+            print()
+            if actual['Penting'].size != pred['Penting'].size:
+                vid_name = video_name + "_" + \
+                    str(100 * round(abs(actual['Penting'].size -
+                        pred['Penting'].size) / actual['Penting'].size, 3))
+            else:
+                vid_name = video_name
+
+            min_length = min(actual['Penting'].size, pred['Penting'].size)
+            c = classification_report(
+                actual['Penting'].iloc[0: min_length], pred['Penting'].iloc[0: min_length], output_dict=True)
+            t_precision = c['True']['precision']
+            t_recall = c['True']['recall']
+            t_f1 = c['True']['f1-score']
+
+            f_precision = c['False']['precision']
+            f_recall = c['False']['recall']
+            f_f1 = c['False']['f1-score']
+            c_df = pd.DataFrame(c).transpose()
+            c_df.to_csv(Path(dst_folder) /
+                        prediction_folder / (vid_name + ".csv"))
+            n += 1
+            mean_ffs = mean_ffs + f_f1
+            mean_frs = mean_frs + f_recall
+            mean_fps = mean_fps + f_precision
+            mean_tfs = mean_tfs + t_f1
+            mean_trs = mean_trs + t_recall
+            mean_tps = mean_tps + t_precision
+            mean_wps = mean_wps + c['weighted avg']['precision']
+            mean_wrs = mean_wrs + c['weighted avg']['recall']
+            mean_wfs = mean_wfs + c['weighted avg']['f1-score']
+
+    print(f"n = {n}")
+    mean_ffs = mean_ffs / n
+    mean_frs = mean_frs / n
+    mean_fps = mean_fps / n
+    mean_tfs = mean_tfs / n
+    mean_trs = mean_trs / n
+    mean_tps = mean_tps / n
+    mean_wps = mean_wps / n
+    mean_wrs = mean_wrs / n
+    mean_wfs = mean_wfs / n
+
+    print("MEAN")
+
+    test = pd.DataFrame({
+        'mean_true_class': [mean_tps, mean_trs, mean_tfs],
+        'mean_false_class': [mean_fps, mean_frs, mean_ffs],
+        'mean_weighted': [mean_wps, mean_wrs, mean_wfs]
+    }, index=['precision', 'recall', 'f1-score']).transpose()
+    print(test)
+    test.to_csv(desc_metrics_dst)
--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import helper
-helper.test_data("500_bst/", "analysis/500_bst/")
-helper.test_data("700_bst/", "analysis/700_bst/")
-helper.test_data("900_bst/", "analysis/900_bst/")
-helper.test_data("1100_bst/", "analysis/1100_bst/")
-helper.test_data("1500_bst/", "analysis/1500_bst/")
-helper.test_data("1700_bst/", "analysis/1700_bst/")
-helper.test_data("1900_bst/", "analysis/1900_bst/")
-helper.test_data("2000_bst/", "analysis/2000_bst/")
-helper.test_data("500_bst_opening/", "analysis/500_bst_opening/")
-helper.test_data("400_bst_opening/", "analysis/400_bst_opening/")
-helper.test_data("400_bst/", "analysis/400_bst/")
+import glob
+# helper.test_data("500_bst/", "analysis/500_bst/")
+# helper.test_data("700_bst/", "analysis/700_bst/")
+# helper.test_data("900_bst/", "analysis/900_bst/")
+# helper.test_data("1100_bst/", "analysis/1100_bst/")
+# helper.test_data("1500_bst/", "analysis/1500_bst/")
+# helper.test_data("1700_bst/", "analysis/1700_bst/")
+# helper.test_data("1900_bst/", "analysis/1900_bst/")
+# helper.test_data("2000_bst/", "analysis/2000_bst/")
+# helper.test_data("500_bst_opening/", "analysis/500_bst_opening/")
+# helper.test_data("400_bst_opening/", "analysis/400_bst_opening/")
+
+paths = glob.glob("inference/*bst*")
+for i, _ in enumerate(paths):
+    paths[i] = paths[i].split('/')[1]
+
+for path in paths:
+    print(path)
+    print()
+    helper.test_data_per_class(path, "analysis/testing/")
+    print()
+    print()
+    print()
